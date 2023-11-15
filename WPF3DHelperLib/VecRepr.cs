6ba482a9651244931723c777576c9bb0b1b9aca1
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-15 21:34:39 +0100

    op

---
+++ WPF3DHelperLib/VecRepr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MML;
+
+namespace MML_VectorFieldVisualizer
+{
+  public class VecRepr
+  {
+    public Vector3Cartesian Pos;
+    public Vector3Cartesian Vec;
+
+    public VecRepr(Vector3Cartesian pos, Vector3Cartesian vec)
+    {
+      Pos = pos;
+      Vec = vec;
+    }
+  }
+}
