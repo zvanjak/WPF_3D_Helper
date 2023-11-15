0c16ede6c4e7e90c9c0fd44d5c7a05394f74ae47
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-15 22:09:45 +0100

    dobaaar

--- WPF3DHelperLib/WorldCameraMouseHelper.cs
+++ WPF3DHelperLib/WorldCameraMouseHelper.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
 
 namespace WPF3DHelperLib
 {
-  internal class WorldCameraMouseHelper
+  public class WorldCameraMouseHelper
   {
+    PerspectiveCamera myPCamera = new PerspectiveCamera();
+
+    Point3D _cameraPos = new Point3D(180, 80, 150);
+    Point3D _lookToPos = new Point3D(0, 0, 0);
+
+    Point3D _startCameraPosRButtonClick;
+
+    private bool _bLButtonDown = false;
+    private bool _bRightButtonDown = false;
+
+    private Point _lastMousePos;
+    private Point _startMouseRButtonClick;
   }
 }
