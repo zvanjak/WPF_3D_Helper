7715470b32f9170c8eaff834c1c77aafe454acdd
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-28 18:10:39 +0100

    ee

--- WPF3DHelperLib/Utils.cs
+++ WPF3DHelperLib/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
 namespace WPF3DHelperLib
@@ -15,6 +16,25 @@ namespace WPF3DHelperLib
 
       ret.Normalize();
       return ret;
+    }
+    public static void DrawCoordSystem(Model3DGroup modelGroup)
+    {
+      double defAxisWidth = 0.5;
+      var axisMaterial = new DiffuseMaterial(new SolidColorBrush(Colors.DarkBlue));
+
+      double axisLen = 300;
+
+      MeshGeometry3D axisX = Geometries.CreateParallelepiped(new Point3D(0, 0, 0), axisLen, defAxisWidth, defAxisWidth);
+      GeometryModel3D axisXModel = new GeometryModel3D(axisX, axisMaterial);
+      modelGroup.Children.Add(axisXModel);
+
+      MeshGeometry3D axisY = Geometries.CreateParallelepiped(new Point3D(0, 0, 0), defAxisWidth, axisLen, defAxisWidth);
+      GeometryModel3D axisYModel = new GeometryModel3D(axisY, axisMaterial);
+      modelGroup.Children.Add(axisYModel);
+
+      MeshGeometry3D axisZ = Geometries.CreateParallelepiped(new Point3D(0, 0, 0), defAxisWidth, defAxisWidth, axisLen);
+      GeometryModel3D axisZModel = new GeometryModel3D(axisZ, axisMaterial);
+      modelGroup.Children.Add(axisZModel);
     }
   }
 }
