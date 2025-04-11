d9252388de8f43d8a822de80741e534cf749f20a
Zvonimr Vanjak <zvonimir.vanjak@gmail.com>
2025-04-11 11:36:35 +0200

    eeee

---
+++ WPF3DHelperLib/Vector2Repr.cs
@@ -0,0 +1,21 @@
+﻿using MML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF3DHelperLib
+{
+  public class Vector2Repr
+  {
+    public Vector2Cartesian Pos;
+    public Vector2Cartesian Vec;
+
+    public Vector2Repr(Vector2Cartesian pos, Vector2Cartesian vec)
+    {
+      Pos = pos;
+      Vec = vec;
+    }
+  }
+}
